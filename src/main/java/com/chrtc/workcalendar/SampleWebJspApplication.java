3067cc482588f321e6326686c06221b649afa6d8
swwxch <slwxch_123@163.com>
2017-12-25 10:32:27 +0800

    3

--- src/main/java/com/chrtc/workcalendar/SampleWebJspApplication.java
+++ src/main/java/com/chrtc/workcalendar/SampleWebJspApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+//开启注解缓存功能
+@EnableCaching
 public class SampleWebJspApplication extends SpringBootServletInitializer {
 
     @Override
